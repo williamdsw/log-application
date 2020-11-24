717b34cf9ac8b9500ec2e4abaeb34b1cf6946760
William Santos <williamdsw@outlook.com>
2020-11-24 16:41:04 -0300

    Refactoring for App Component (Navbar)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  // FIELDS
+  // Fields
 
   public title = 'log-application';
   public navbarLinks = [
@@ -16,4 +16,23 @@ export class AppComponent {
     { text: 'Batch Log', href: 'batch-log' },
     { text: 'Search Logs', href: 'search-logs' },
   ];
+  public showNavbarLinks = true;
+
+  // Hookers
+
+  ngOnInit() {
+    this.checkWhatDeviceIs();
+  }
+
+  // Helper Functions
+
+  public toggleShowNavbarLinks(): void {
+    this.showNavbarLinks = !this.showNavbarLinks;
+  }
+
+  public checkWhatDeviceIs(): void {
+    const userAgent = navigator.userAgent;
+    const mobilePattern = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i;
+    this.showNavbarLinks = !mobilePattern.test(userAgent);
+  }
 }
